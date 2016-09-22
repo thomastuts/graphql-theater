cad80b4121f37a036a0d0288948a73b21232c56c
thomastuts <thomas@thomastuts.com>
2016-09-22 13:12:33 +0200

    Reference the appropriate ID columns in movies/genres junction table

--- database/migrations/20160922122424_movies_genres.js
+++ database/migrations/20160922122424_movies_genres.js
@@ -1,7 +1,7 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('movies_genres', t => {
-    t.integer('movie_id').notNull();
-    t.integer('genre_id').notNull();
+    t.integer('movie_id').unsigned().references('movies.id');
+    t.integer('genre_id').unsigned().references('genres.id');
   });
 };
 
